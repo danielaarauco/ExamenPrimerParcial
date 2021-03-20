c48f7b56c5ef06168e96d5ad4f9b51e2d2b9c396
Daniela Arauco <79011580+danielaarauco@users.noreply.github.com>
2021-03-20 10:31:06 -0400

    SE CREO EL MODELO PRODUCTO Y CURRENCY

---
+++ SolParcialArauco/Parcial1_DanielaAraucoPadilla/Models/Currency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1_DanielaAraucoPadilla.Models
+{
+    public class Currency
+    {
+        [Key]
+        public string Code { get; set; }
+        [Required]
+        public double Name { get; set; }
+        [Required]
+        [StringLength(5,MinimumLength = 1, ErrorMessage = "ingrese de 1 a 5 caracteres")]
+        public string Symbol { get; set; }
+
+    }
+}
---
+++ SolParcialArauco/Parcial1_DanielaAraucoPadilla/Models/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1_DanielaAraucoPadilla.Models
+{
+    public class Producto
+    {
+        [Key]
+        [StringLength(99999, MinimumLength = 1, ErrorMessage= "Ingrese entre 1 a 99999 caracteres")]
+        public string ProductID { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 1, ErrorMessage = "Ingrese entre 1 a 30 caracteres")]
+        public string Description { get; set; }
+        [Required]
+        public Currency Price { get; set; }
+        public DateTime LastBuy { get; set; }
+
+    }
+}
